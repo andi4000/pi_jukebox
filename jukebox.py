b0593173dc9278b8500f1ffa9bdf6194025f7230
ariandy <ariandyy@gmail.com>
2020-11-27 09:38:30 +0100

    first rewrite with RPi.GPIO, works

--- jukebox.py
+++ jukebox.py
@@ -1,36 +1,72 @@
 #!/usr/bin/env python3
 
-from gpiozero import Button
+import RPi.GPIO as GPIO
 from signal import pause
 from time import sleep
 import logging
 
-import vlc
+## IO Definitions BEGIN
+PIN_TAILSWITCH = 2
+PIN_BUTTONS = [
+        7,  # 0
+        8,  # 1
+        11, # 2
+        9,  # 3
+        10, # 4
+        15, # 5
+        14, # 6
+        3   # 7
+        ]
 
-SONG = "music/beetje_bang.mp3"
+DEFAULT_BOUNCE_TIME_MS = 200
 
-logging.basicConfig(level=logging.DEBUG)
+PIN_LEDS = [
+        17, # 0
+        18, # 1
+        27, # 2
+        22, # 3
+        23, # 4
+        24, # 5
+        25, # 6
+        4 # 7
+        ]
 
+## IO Definitions END
+
+## Songs BEGIN
+songs = [
+        "music/beetje_bang.mp3",
+        "music/fitlala.mp3",
+        "music/hello_dangdut.mp3",
+        "music/maja_tanz.mp3",
+        "music/skj88.mp3"
+        ]
+## Songs END
+
+logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s",
+        level=logging.DEBUG)
 logging.info("initializing")
 
-vlc_instance = vlc.Instance('--aout=alsa')
+led_states = [False] * len(PIN_LEDS)
 
-player = vlc_instance.media_player_new()
-player.audio_set_volume(100)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-def action_1():
-    if player.is_playing():
-        logging.info("stopping playback")
-        player.stop()
-    else:
-        logging.info("loading media and playing")
-        player.set_media(vlc_instance.media_new(SONG))
-        player.stop()
-        player.play()
+GPIO.setup(PIN_LEDS, GPIO.OUT)
+GPIO.output(PIN_LEDS, led_states)
+
+GPIO.setup(PIN_BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+logging.info("GPIO Pins initialized")
+
+def cb(channel):
+    idx = PIN_BUTTONS.index(channel)
+    logging.debug(f"button press {idx}")
+    led_states[idx] = not led_states[idx]
+    GPIO.output(PIN_LEDS[idx], led_states[idx])
 
 
-btn = Button(18)
-btn.when_pressed = action_1
+for i in range(len(PIN_LEDS)):
+    GPIO.add_event_detect(PIN_BUTTONS[i], GPIO.RISING,
+            callback=cb, bouncetime=DEFAULT_BOUNCE_TIME_MS)
 
-logging.info("ready")
 pause()
