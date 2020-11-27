7fa60165fb9186ac95c48cfc1abf4c7ad5c1cc9b
ariandy <ariandyy@gmail.com>
2020-11-27 09:43:58 +0100

    add vlc init routine

--- jukebox.py
+++ jukebox.py
@@ -45,7 +45,8 @@ songs = [
 
 logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s",
         level=logging.DEBUG)
-logging.info("initializing")
+
+logging.info("Initializing GPIO")
 
 led_states = [False] * len(PIN_LEDS)
 
@@ -58,7 +59,25 @@ GPIO.output(PIN_LEDS, led_states)
 GPIO.setup(PIN_BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 logging.info("GPIO Pins initialized")
 
+logging.info("Initializing VLC")
+import vlc
+vlc_instance = vlc.Instance("--aout=alsa")
+player = vlc_instance.media_player_new()
+player.audio_set_volume(100)
+logging.info("VLC initialized")
+
+g_active_song_idx = None
+
+def play_song(song_path):
+    logging.info(f"playing media: {song_path}")
+    player.stop()
+    player.set_media(vlc_instance.media_new(song_path))
+    player.play()
+
+
 def cb(channel):
+    global g_active_song_idx
+
     idx = PIN_BUTTONS.index(channel)
     logging.debug(f"button press {idx}")
     led_states[idx] = not led_states[idx]
