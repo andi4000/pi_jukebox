8c3f36ee63d193c31e89ab932e49d410be7cc149
ariandy <ariandyy@gmail.com>
2021-02-18 02:16:38 +0100

    fix entry point for launch script

--- pi_jukebox/__main__.py
+++ pi_jukebox/__main__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import sys
 from .pi_jukebox import PiJukebox
 
-if __name__ == "__main__":
+def main():
     app = PiJukebox()
     app.init()
     app.run()
+
+if __name__ == "__main__":
+    sys.exit(main())
